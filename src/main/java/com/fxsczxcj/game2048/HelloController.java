d06377a50dc85839924ee735ac412de3b0cb6a24
FXSC <fxsczxcj@163.com>
2022-09-28 00:00:26 +0800

    Start

---
+++ src/main/java/com/fxsczxcj/game2048/HelloController.java
@@ -0,0 +1,18 @@
+package com.fxsczxcj.game2048;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * The type Hello controller.
+ */
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.fxsczxcj.game {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+//    requires com.almasb.fxgl.all;
+
+    opens com.fxsczxcj.game2048 to javafx.fxml;
+    exports com.fxsczxcj.game2048;
+}
